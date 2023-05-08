0f739ab8d73249b9f2726d02098c565fe1a48f2a
FacundoMajda <facundomajda13@gmail.com>
2023-05-08 11:51:12 -0300

    Creacion de carpeta scripts y script de manipulacion de DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
 console.log("Hello world!");
+
+// Esto portfolio cuenta con js para manipular el DOM.
+// 1er Parcial TLP1
+//Facundo Majda
+
+
+
+
+// Elementor Toggler 
+
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+// sidebar variables
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+// sidebar toggle functionality for mobile
+sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+
