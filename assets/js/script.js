0fdc70846e9751222d7b7698a01c1149f96346da
FacundoMajda <facundomajda13@gmail.com>
2023-05-08 21:16:03 -0300

    Se actualizaron iconos, se agregó una tonelada de css para  los mQuery para responsividad

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,40 @@
 console.log("Hello world!");
 
-// Esto portfolio cuenta con js para manipular el DOM.
+// Este portfolio cuenta con js para manipular el DOM.
 // 1er Parcial TLP1
-//Facundo Majda
+//  Facundo Majda
 
+// Elementor Toggler
 
-
-
-// Elementor Toggler 
-
-const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
+const elementToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
 
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 // sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+sidebarBtn.addEventListener("click", function () {
+  elementToggleFunc(sidebar);
+});
 
+// 1
+// testimonials variables
+const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+const modalContainer = document.querySelector("[data-modal-container]");
+const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+// modal variable
+const modalImg = document.querySelector("[data-modal-img]");
+const modalTitle = document.querySelector("[data-modal-title]");
+const modalText = document.querySelector("[data-modal-text]");
+
+// modal toggle function
+const testimonialsModalFunc = function () {
+  modalContainer.classList.toggle("active");
+  overlay.classList.toggle("active");
+};
+
+//2
